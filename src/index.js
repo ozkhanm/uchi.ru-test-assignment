aeed123a7125006d82bcbcf88829752c01bba21c
Ozkhan Mamedov <ozhanm96@yandex.ru>
2022-07-03 18:54:21 +0300

    вернул строгий режим

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import GlobalStyle from "./globalStyle";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <>
+  <React.StrictMode>
     <Normalize/>
     <GlobalStyle/>
     <App/>
-  </>
+  </React.StrictMode>
 );
